cb263fcf69f2f7bf677d866cd4bbe163a8a41a6d
yoy159 <yoy159@naver.com>
2023-04-03 11:02:44 +0900

    23-04-03

--- c++/1_BronzeV_킹퀸룩비숍나이트폰.cpp
+++ c++/1_BronzeV_킹퀸룩비숍나이트폰.cpp
@@ -5,4 +5,24 @@ using namespace std;
 
 int main()
 {
+    int chess[6];
+    int chess2[6] = {1, 1, 2, 2, 2, 8};
+    int result[6];
+
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> chess[i];
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        result[i] = chess2[i] - chess[i];
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << result[i] << endl;
+    }
+
+    return 0;
 }
