7624d468bf4ed925891a80a4265065b70657e4b0
yoy159 <yoy159@naver.com>
2023-04-06 12:00:25 +0900

    23-04-06

--- BronzeV/10_BronzeV_WhoIsInTheMiddle.cpp
+++ BronzeV/10_BronzeV_WhoIsInTheMiddle.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-
+    int a, b, c;
+    cin >> a >> b >> c;
+    int list[3] = {a, b, c};
+    sort(list, list + 3);
+    cout << list[1] << endl;
     return 0;
 }
